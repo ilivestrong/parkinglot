5f9026f1802a7953a40f4cbc8d80b477dd0dfb5b
Deepak Pathak <imdeepakpathak@gmail.com>
2025-01-24 09:33:00 +0530

    read lot capacity from env

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+const (
+	ParkingLotCapacityEnvKey      = "PARKING_LOT_CAPACITY"
+	DefaultParkingLotCapacity int = 100
+)
+
 func main() {
+
+	log.Println("Initializing Car Parking Lot system")
+
+	parkingLotCapacity := DefaultParkingLotCapacity
+	v, exists := os.LookupEnv(ParkingLotCapacityEnvKey)
+	if exists {
+		if capacity, err := strconv.Atoi(v); err != nil {
+			parkingLotCapacity = capacity
+		}
+	}
+	fmt.Println(parkingLotCapacity)
 
 }

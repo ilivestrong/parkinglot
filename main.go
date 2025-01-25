98b1b069c8b06425d4e56838cf56b0e7034651d4
Deepak Pathak <imdeepakpathak@gmail.com>
2025-01-25 11:17:51 +0530

    feat: implement interactive mode

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/ilivestrong/internal/types"
+)
+
+const (
+	ModeInteractive = "interactive"
+	ModeFileBased   = "filebased"
 )
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
 	defer cancel()
 
-	cmdBuilder := types.CommandBuilder{}
+	cmdBuilder := types.NewCommandBuilder(ModeFileBased)
 
 	args := os.Args[1:]
 	if len(args) > 0 {
@@ -24,11 +32,50 @@ func main() {
 		}
 		executeCommands(ctx, commands)
 		return
+	} else {
+		cmdBuilder := types.NewCommandBuilder(ModeInteractive)
+		startInteractiveMode(ctx, cmdBuilder)
 	}
 }
 
 func executeCommands(ctx context.Context, commands []types.Commander) {
 	for _, command := range commands {
 		command.Execute(ctx)
+	}
+}
+
+func startInteractiveMode(ctx context.Context, cb *types.CommandBuilder) {
+	parkingLotCreated := false
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		input, _ := reader.ReadString('\n')
+		tokens := strings.Split(input, " ")
+		if len(tokens) < 1 {
+			fmt.Println("invalid command.....")
+			continue
+		}
+
+		trimmedToken := strings.TrimSpace(tokens[0])
+		if strings.ToLower(trimmedToken) == "exit" {
+			return
+		}
+
+		cmd := cb.ParseCommand(trimmedToken, tokens[1:]...)
+		if cmd == nil {
+			continue
+		}
+
+		if trimmedToken == types.TokenForCreateParkingLot {
+			parkingLotCreated = true
+		}
+
+		if !parkingLotCreated && trimmedToken != types.TokenForCreateParkingLot {
+			fmt.Printf("\ninvalid, please create a parking lot first\n\n")
+			continue
+		}
+
+		cmd.Execute(ctx)
+		fmt.Printf("\n\n")
 	}
 }
